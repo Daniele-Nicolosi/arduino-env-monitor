69e8db00aa15b3aba277e474e8aec4f3c9ae3c6e
Daniele <danielenicolosi03@gmail.com>
2025-10-02 11:53:34 +0200

    aggiorna commenti in uart.c e uart.h

--- avr_common/uart/uart.c
+++ avr_common/uart/uart.c
@@ -1,65 +1,77 @@
 #include "uart.h"
-#include <avr/io.h>
-#include <stdio.h>
 
-// ---- Configurazione UART ----
+// --- Includes ---
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+// --- Macros and Defines ---
 #define BAUD 19200
 #define MYUBRR F_CPU/16/BAUD-1
 
-// ---- Stream per printf ----
+// --- Function Prototypes ---
+void usart_init(uint16_t ubrr);
+char usart_getchar(void);
+void usart_putchar(char data);
+void usart_pstr(char *s);
+unsigned char usart_kbhit(void);
+int usart_putchar_printf(char var, FILE *stream);
+
 static FILE mystdout = FDEV_SETUP_STREAM(usart_putchar_printf, NULL, _FDEV_SETUP_WRITE);
 
-// ---- Inizializzazione ----
+// --- USART Related ---
 void usart_init(uint16_t ubrr) {
-    // Imposta baud rate
+    // Set baud rate
     UBRR0H = (uint8_t)(ubrr >> 8);
     UBRR0L = (uint8_t)ubrr;
 
-    // Frame: 8 bit, 1 stop, no parity
-    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00);
-
-    // Abilita TX e RX
-    UCSR0B = _BV(RXEN0) | _BV(TXEN0);
+    UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); // 8-bit data
+    UCSR0B = _BV(RXEN0) | _BV(TXEN0) | _BV(RXCIE0); // Enable RX and TX
 }
 
-// ---- Funzioni di base ----
 void usart_putchar(char data) {
-    // attesa buffer libero
-    while (!(UCSR0A & _BV(UDRE0)));
+    // Wait for empty transmit buffer
+    while (!(UCSR0A & (_BV(UDRE0))));
+    // Start transmission
     UDR0 = data;
 }
 
 char usart_getchar(void) {
-    // attesa ricezione
-    while (!(UCSR0A & _BV(RXC0)));
+    // Wait for incoming data
+    while (!(UCSR0A & (_BV(RXC0))));
+    // Return the data
     return UDR0;
 }
 
 unsigned char usart_kbhit(void) {
-    // ritorna 1 se un carattere è disponibile
-    return (UCSR0A & (1 << RXC0)) ? 1 : 0;
+    // Return nonzero if char waiting (polled version)
+    unsigned char b = 0;
+    if (UCSR0A & (1 << RXC0)) b = 1;
+    return b;
 }
 
 void usart_pstr(char *s) {
-    // stampa stringa
+    // Loop through entire string
     while (*s) {
-        usart_putchar(*s++);
+        usart_putchar(*s);
+        s++;
     }
 }
 
-// ---- Integrazione con printf ----
+// Called by printf as a stream handler
 int usart_putchar_printf(char var, FILE *stream) {
-    // traduce \n in \r\n
-    if (var == '\n') {
-        usart_putchar('\r');
-    }
+    // Translate \n to \r for br@y++ terminal
+    if (var == '\n') usart_putchar('\r');
     usart_putchar(var);
     return 0;
 }
 
 void printf_init(void) {
-    // collega printf a UART
     stdout = &mystdout;
-    // inizializza UART
+
+    // Fire up the USART
     usart_init(MYUBRR);
 }
+
+
--- avr_common/uart/uart.h
+++ avr_common/uart/uart.h
@@ -1,14 +1,16 @@
 #pragma once
 
-#include <stdint.h>
+#include <stdio.h>
 
-// ---- API UART ----
-void usart_init(uint16_t ubrr);
+// --- Function Prototypes ---
+
+// Inizializza printf sulla UART
+void printf_init(void);
+
+// Funzioni di base della UART
 void usart_putchar(char data);
 char usart_getchar(void);
 unsigned char usart_kbhit(void);
-void usart_pstr(char *s);
-
-// ---- Supporto printf ----
 int usart_putchar_printf(char var, FILE *stream);
-void printf_init(void);
+
+
